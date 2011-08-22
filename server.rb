8921d933393268912d09e88bb049d0134e0162d9
lizconlan <lizconlan@gmail.com>
2011-08-22 16:58:29 +0100

    more deliberate-looking HTML

--- server.rb
+++ server.rb
@@ -5,9 +5,17 @@ require 'sass'
 
 require 'lib/repo_list'
 
+before do
+  @username = "lizconlan"
+end
+
+get "/styles/style.css" do
+  sass :style
+end
+
 get "/" do
   #cache for an hour
   response.headers['Cache-Control'] = 'public, max-age=3600'
-  @repos = RepoList.new("lizconlan")
+  @repos = RepoList.new(@username)
   haml :index
 end
