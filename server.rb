8f418cfdaaf4113918e0880bb5e7bff82bf60f49
lizconlan <lizconlan@gmail.com>
2011-08-23 11:33:36 +0100

    using config.yml for better flexibility

--- server.rb
+++ server.rb
@@ -2,12 +2,13 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'yaml'
 
 require './lib/github_repo_list'
 require './lib/scraperwiki_scraper_list'
 
 before do
-  @username = "lizconlan"
+  @config = YAML.load(File.read('config/config.yml'))
 end
 
 get "/styles/style.css" do
@@ -17,7 +18,14 @@ end
 get "/" do
   #cache for 2 hours
   response.headers['Cache-Control'] = 'public, max-age=7200'
-  @repos = GithubRepoList.new(@username)
-  @scrapers = ScraperwikiScraperList.new(@username)
+  @config["code_hosts"].each do |host|
+    case host["host"]
+      when "github"
+        @repos = GithubRepoList.new(host["username"])
+      when "scraperwiki"
+        @scrapers = ScraperwikiScraperList.new(host["username"])
+    end
+  end
+  
   haml :index
 end
