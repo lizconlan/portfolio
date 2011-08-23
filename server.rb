2ee40826aa80efaf3773d266cce468ac847f783c
lizconlan <lizconlan@gmail.com>
2011-08-23 11:58:43 +0100

    more useful config.yml; added nav links

--- server.rb
+++ server.rb
@@ -18,12 +18,18 @@ end
 get "/" do
   #cache for 2 hours
   response.headers['Cache-Control'] = 'public, max-age=7200'
+  
+  @section_links = []
+  
   @config["code_hosts"].each do |host|
     case host["host"]
       when "github"
         @repos = GithubRepoList.new(host["username"])
       when "scraperwiki"
         @scrapers = ScraperwikiScraperList.new(host["username"])
+    end
+    host["sections"].each do |section|
+      @section_links << {"link" => "#{host["host"]}_#{section["type"]}", "text" => section["text"]}
     end
   end
   
