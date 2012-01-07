86d7b75b61734c4dccbabd8a691a1878646fad45
lizconlan <lizconlan@gmail.com>
2012-01-07 13:19:33 +0000

    ignoring oauth things

--- lib/github_repo_list.rb
+++ lib/github_repo_list.rb
@@ -5,37 +5,26 @@ class GithubRepoList < HostedCodeList
   
   def initialize(username)
     @username = username
+    @token = ENV['gh_token'] || YAML::load(File.read("config/oauth.yml"))[:access_token]
     get_data()
   end
   
   private
     def get_data
-      @all = call_api("https://api.github.com/users/#{@username}/repos?type=public")
+      @all = call_api("https://api.github.com/users/#{@username}/repos?type=public?access_token=#{@token}")
       
       @contributor = []
       
       @own = @all.dup
       @own.delete_if {|x| x["fork"] == true}
-      @own.delete_if {|x| x["owner"]["login"] != @username}
       
       @forks = @all.dup
       @forks.delete_if {|x| x["fork"] == false}
       
-      @watched = call_api("https://api.github.com/users/#{@username}/watched")
-      
-      #ignore your own stuff that you "watch"
-      @watched.delete_if {|x| x["owner"]["login"] == @username}
-      
-      @watched.each do |repo|
-        process_repo_contribs(repo)
-      end
-      
-      #being a watcher and a contributor isn't massively useful, remove the duplicates from @watched
-      contrib_project_names = @contributor.collect {|x| x["name"]}
-      @watched.delete_if {|x| contrib_project_names.include?(x["name"])}
+      @contributor = call_api("https://api.github.com/users/#{@username}/repos?type=member&access_token=#{@token}")
       
       #include stuff from organisations
-      @organisations = call_api("https://api.github.com/users/#{@username}/orgs")
+      @organisations = call_api("https://api.github.com/users/#{@username}/orgs?access_token=#{@token}")
       @organisations.each do |org|
         org_repos = call_api("https://api.github.com/orgs/#{org["login"]}/repos")
         org_repos.each do |repo|
